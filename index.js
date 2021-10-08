e39c610081d4eff14a481279b204039e082dd72d
Jimikki <90453961+Jimikki@users.noreply.github.com>
2021-10-08 14:26:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function arvoNumero(alaraja, ylaraja) {
 
 app.get('/', funktion (req, res) {
     let NUMEROLISTA = [
-        arvoNumero(0,5)
-        arvoNumero(10,15)
-        arvoNumero(100,500)
-        arvoNumero(10,20)
-        arvoNumero(0,5)
+        arvoNumero(0,5),
+        arvoNumero(10,15),
+        arvoNumero(100,500),
+        arvoNumero(10,20),
+        arvoNumero(0,5),
     ]
 
     res.json(NUMEROLISTA)
