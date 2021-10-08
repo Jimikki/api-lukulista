6e6f58d4f098038ade41a0e559d0fbd2d82086ef
Jimikki <90453961+Jimikki@users.noreply.github.com>
2021-10-08 14:15:57 +0300

    Numerolista

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+function arvoNumero(alaraja, ylaraja) {
+    return Math.floor(ylaraja * Math.random()) + alaraja
+}
+
+let NUMEROLISTA = [
+    arvoNumero(0,5)
+    arvoNumero(10,15)
+    arvoNumero(100,500)
+    arvoNumero(10,20)
+    arvoNumero(0,5)
+]
+
 app.get('/', funktion (req, res) {
-    res.json([1, 5, 8, 2, 5])
+    res.json(NUMEROLISTA)
 })
 
 app.listen(PORT)
